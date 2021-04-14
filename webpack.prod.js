b3dcb347f8cc0121041ef75ea2e9bd0cfd27a4fd
Riccardo Foti <riccardofoti97@gmail.com>
2021-04-14 12:12:51 +0100

    Added prod and dev scripts

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,7 +3,7 @@ const webpack = require('webpack')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: './src/client/index.js',
     devtool: 'source-map',
     module: {
